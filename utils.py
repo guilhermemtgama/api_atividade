dfda58ef6bf16907d215efd455d2a82be3c12bad
guilhermemtgama <101999593+guilhermemtgama@users.noreply.github.com>
2022-03-29 21:42:46 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from models import Pessoas
 
 def insere_pessoas():
-    pessoa = Pessoas(nome = 'Guilherme',idade = 27)
+    pessoa = Pessoas(nome = 'jose',idade = 27)
     print(pessoa, pessoa.idade)
     pessoa.save()
 
@@ -9,15 +9,15 @@ def insere_pessoas():
 def consulta():
     pessoa = Pessoas.query.all()
     print(pessoa)
-    pessoa =Pessoas.query.filter_by(nome = 'Geovani').first()
+    pessoa =Pessoas.query.filter_by(nome = 'joao').first()
     print(pessoa.idade)
 
 def altera_pessoa():
-    pessoa = Pessoas.query.filter_by(nome = 'Leticia').first()
+    pessoa = Pessoas.query.filter_by(nome = 'maria').first()
     pessoa.idade = 24
     pessoa.save()
 
 if __name__ == '__main__':
     insere_pessoas()
-    # consulta()
-    # altera_pessoa()
+    consulta()
+    altera_pessoa()
